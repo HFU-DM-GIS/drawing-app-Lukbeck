aa8f7506d0cc30c39b6fc2cbeb7fba00eaa8a775
Adrian-1405 <128720790+Adrian-1405@users.noreply.github.com>
2023-04-24 12:53:52 +0200

    Lukas u. Adrian

--- script.js
+++ script.js
@@ -8,20 +8,22 @@ const ctx = canvas.getContext("2d");
 
 let size = 10;
 let isPressed = false;
-colorEl.value = "black";
 let color = colorEl.value;
 let x;
 let y;
+
 canvas.addEventListener("mousedown", (e) => {
   isPressed = true;
   x = e.offsetX;
   y = e.offsetY;
 });
+
 canvas.addEventListener("mouseup", (e) => {
   isPressed = false;
   x = undefined;
   y = undefined;
 });
+
 canvas.addEventListener("mousemove", (e) => {
   if (isPressed) {
     const x2 = e.offsetX;
@@ -31,6 +33,12 @@ canvas.addEventListener("mousemove", (e) => {
     x = x2;
     y = y2;
   }
+});
+
+canvas.addEventListener("mouseleave", (e) => {
+  isPressed = false;
+  x = undefined;
+  y = undefined;
 });
 
 function drawCircle(x, y) {
@@ -48,27 +56,34 @@ function drawLine(x1, y1, x2, y2) {
   ctx.lineWidth = size * 2;
   ctx.stroke();
 }
+
 function updateSizeOnScreen() {
   sizeEL.innerText = size;
 }
+
 increaseBtn.addEventListener("click", () => {
-  size + 5;
-  if (size > 50) {
-    size = 50;
+  size += 5;
+  if (size > 100) {
+    size = 100;
   }
   updateSizeOnScreen();
 });
+
 decreaseBtn.addEventListener("click", () => {
-  size - 5;
+  size -= 5;
   if (size < 5) {
     size = 5;
   }
   updateSizeOnScreen();
 });
-colorEl.addEventListener("change", (e) => (color = e.target.value));
-clearEl.addEventListener("click", () =>
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-);
 
+colorEl.addEventListener("change", (e) => {
+  color = e.target.value;
+});
 
+clearEl.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+updateSizeOnScreen();
 
