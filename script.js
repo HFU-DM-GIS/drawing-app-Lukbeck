9abe8b096edc3ee83d948f888cc5f7f7a9d69260
Adrian-1405 <128720790+Adrian-1405@users.noreply.github.com>
2023-05-22 13:29:48 +0200

    A5

--- script.js
+++ script.js
@@ -92,5 +92,23 @@ window.onload = function() {
   });
 };
 
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+  document.body.style.backgroundImage = color;
+}
+
+randomColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomColor();
+  colorEl.value = randomColor;
+ color = randomColor;
+ document.body.style.backgroundColor = randomColor;
+ ;
+});
+
 updateSizeOnScreen();
 
